7473ae18741ec0b75a8d248f833464f213ae6f6a
Rohan Singh <rohansingh.97@outlook.com>
2024-03-01 10:39:53 +0530

    Read and Write
    Reading and Writing in Node Server.

---
+++ read-write.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>My First Form</title></head>');
+        res.write('<body><form action="/message" method = "POST"><input type ="text" name="message"><button type="submit">Submit</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'Dummy');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Node Server</title></head>');
+    res.write('<body><h1>Hello World Welcome to Universe</h1></body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000);
